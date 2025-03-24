9611ebe54a428f893f2dc538c8d4a57130ddff87
p6laris <yunissurchy@gmail.com>
2025-03-25 00:58:43 +0300

    Unused code

--- AraratMorse/Program.cs
+++ AraratMorse/Program.cs
@@ -9,12 +9,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
 builder.Services.AddFluxor(o => { o.ScanAssemblies(typeof(Program).Assembly); });
-
 builder.Services.AddClipboard();
 builder.Services.AddTransient<IValidator<Settings>, SettingsValidator>();
-
 
 await builder.Build().RunAsync();
